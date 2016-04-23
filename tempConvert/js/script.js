cf3b5bcdf624c3f10ef0c0de46bc64d2ba9f50c8
stevenaldous <stevenaldous@gmail.com>
2016-04-23 11:31:23 -0700

    first commit

---
+++ tempConvert/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded',function(){
+
+  document.getElementById('toC').addEventListener('click',function(){
+    var int = parseInt(document.getElementById('celsius').value);
+    var toC = function(num){
+      return parseInt((num -32)/(9/5))
+    }
+    document.getElementById('temp').innerHTML = toC(int);
+  });
+  document.getElementById('clear').addEventListener('click',function(){
+    document.getElementById('celsius').value = '';
+    document.getElementById('temp').innerHTML = '--'
+  });
+ });
