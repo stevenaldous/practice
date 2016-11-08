cd176b4071de2de41410293f33b570e04ff49f34
stevenaldous <stevenaldous@gmail.com>
2016-11-08 14:49:26 +0800

    some php practice

--- php_practice/index.php
+++ php_practice/index.php
@@ -1,1 +1,13 @@
-<?php php_info(); ?>
+<?php $var = 'PHP Tutorial';  ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>My <?php echo $var; ?></title>
+</head>
+<body>
+<h3><?php echo $var; ?></h3>
+
+<p>PHP, an acronym for Hypertext Preprocessor, is a widely-used open source general-purpose scripting language. It is a cross-platform, HTML embedded server-side scripting language and is especially suited for web development. For more info, go to <a href="#"><?php echo $var; ?></a></p>
+
+</body>
+</html>
